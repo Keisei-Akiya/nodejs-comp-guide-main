f759cd633a10b648f8627e7ea53ed93e2cf43e3d
Keisei Akiya <keisei.akiya@gmail.com>
2024-09-01 23:42:05 +0900

    npmを学んだ

--- 03_npm/010_intro/start/main.mjs
+++ 03_npm/010_intro/start/main.mjs
@@ -1,2 +1,9 @@
+import cloneDeep from "lodash/cloneDeep.js";
+
 const original = { prop: { nested: "value" } };
 // オブジェクトの複製
+const cloned = cloneDeep(original);
+
+cloned.prop.nested = "new value";
+
+console.log(original, cloned);
