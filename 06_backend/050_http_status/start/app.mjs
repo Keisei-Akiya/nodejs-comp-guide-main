85f05ef08da7cc56623dcb840af6891f54fa0e83
Keisei Akiya <keisei.akiya@gmail.com>
2024-09-10 21:36:50 +0900

    first commit

--- 06_backend/050_http_status/start/app.mjs
+++ 06_backend/050_http_status/start/app.mjs
@@ -1,13 +1,34 @@
 import * as http from "http";
 
-const server = http.createServer(function(req, res) {
-    console.log(req.url);
-    res.writeHead(200, {'content-type': 'text/html; charset=UTF-8'});
-    if(req.url === '/hello') {
-        res.end(`<h1>こんにちは</h1>`)
-    } else if(req.url === '/bye') {
-        res.end('bye');
-    }
+/**
+ * HTTP status codes:
+ *
+ * 200's: success.
+ *  201, 200
+ *
+ * 300's: redirect.
+ *  301, 302, 304
+ *
+ * 400's: client error.
+ *  401, 403, 404
+ *
+ * 500's: server error.
+ *  500: unknown error,
+ *  503: service down
+ *
+ * see https://developer.mozilla.org/ja/docs/Web/HTTP/Status
+ */
+
+const server = http.createServer(function (req, res) {
+  console.log(req.url);
+  res.writeHead(302, { location: "/redirected" });
+  if (req.url === "/hello") {
+    res.end(`<h1>こんにちは</h1>`);
+  } else if (req.url === "/bye") {
+    res.end("bye");
+  } else {
+    res.end(re1.url);
+  }
 });
 
 server.listen(8080);
